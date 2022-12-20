79562c200227a3bb96b64713b052cd0278868ba9
Philippe Martens <palmartens@ziggo.nl>
2022-12-20 10:18:10 +0000

    import external scritp files

--- src/app/products/product.component.ts
+++ src/app/products/product.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+declare const genRandomNumbers: any;
 
 @Component({
     selector: 'app-product',
@@ -7,5 +8,6 @@ import { Component } from "@angular/core";
 })
 
 export class ProductComponent {
-    showDiv = false;
+    showDiv = true;
+    rNum = <[]>genRandomNumbers();
 }

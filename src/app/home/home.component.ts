5a992c89dcebf2dd41f958c26b6562ca1315ade1
Philippe Martens <palmartens@ziggo.nl>
2022-12-21 14:59:39 +0000

    profile service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProfileService } from '../profile.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-  
+export class HomeComponent implements OnInit {
+  public isLoggedIn: boolean = false;
 
-  constructor() {
+
+  constructor(private profileService: ProfileService) {
     
+  }
+
+  ngOnInit() {
+    this.isLoggedIn = this.profileService.loggedInStatus();
   }
 
 
